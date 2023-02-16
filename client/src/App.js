9a9827899812945b1dd5af4da48f8778a283c31e
Alexandra Delarosa <121692506+alex-delar@users.noreply.github.com>
2023-02-15 22:56:05 -0600

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import ListHeader from "./components/ListHeader"
+import { useEffect } from 'react'
+
+const App = () => {
+
+  const getData = async () => {
+    const userEmail = 'alex@test.com'
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+      const json = await response.json()
+      console.log(json)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => getData, [])
+
+  return (
+    <div className='app'>
+      <ListHeader listName={'🏝️ Holiday tick list'} />
+    </div>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const PORT = process.env.PORT ?? 8000
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const pool = require('./db')
+
+app.use(cors())
+//get all todos                                
+app.get('/todos', async (req, res) => {
+    console.log(req)
+    const userEmail = req.params
+
+    try {
+        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
+        res.json(todos.rows)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+app.listen(PORT, ()=> console.log(`Server running on PORT ${PORT}`))
